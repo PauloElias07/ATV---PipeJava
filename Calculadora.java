f6bb72afaed5815c7119f2c25e61a566ad7fd9b9
paulo <paulo.elias@fatec.sp.gov.br>
2026-08-26 09:18:19 -0300

    Calculadora/Main/InicioYamal

---
+++ Calculadora.java
@@ -0,0 +1,17 @@
+public class Calculadora {
+    public static int resultaSoma(int a, int b){
+        return a + b;
+    }
+
+    public static int resultaSub(int a, int b){
+        return a - b;
+    }
+
+    public static int resultaMulti(int a, int b){
+        return a * b;
+    }
+
+    public static int resultaDivi(int a, int b){
+        return a / b;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main{
+    public static void main(String[] args){
+
+        int soma = Calculadora.resultaSoma(10, 10);
+        int sub = Calculadora.resultaDivi(10, 10);
+        int multi = Calculadora.resultaMulti(10, 10);
+        int divi = Calculadora.resultaDivi(10, 10);
+
+
+    System.out.println("O resultado da Soma é: " + soma);
+    
+    System.out.println("O resultado da Subtração é: " + sub);
+
+    System.out.println("O resultado da Multiplicação é: " + multi);
+
+    System.out.println("O resultado da Divisão é:" + divi);
+    }
+}
