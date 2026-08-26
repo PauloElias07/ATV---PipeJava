43bff5f7d32974abc281437a0e53e75d7ebe3eae
paulo <paulo.elias@fatec.sp.gov.br>
2026-08-26 11:19:25 -0300

    ajuste métodos

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main{
     public static void main(String[] args){
 
         int soma = Calculadora.resultaSoma(10, 10);
-        int sub = Calculadora.resultaDivi(10, 10);
+        int sub = Calculadora.resultaSub(10, 10);
         int multi = Calculadora.resultaMulti(10, 10);
         int divi = Calculadora.resultaDivi(10, 10);
 
